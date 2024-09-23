23f82cda92306d0260db94aa41dc071a99a658a8
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-09-23 21:14:16 +0700

    add: base module for category

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { APP_PIPE } from '@nestjs/core';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [AuthModule, UserModule, PostModule, CategoryModule],
   controllers: [AppController],
   providers: [
     AppService,
